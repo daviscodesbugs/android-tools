e33a7616d276d7b96ab22682f15b2392f2b2e5bc
daviscodesbugs <daviscodesbugs@gmail.com>
2019-06-24 09:51:54 -0600

    Add dialog and adapter methods to interface util

--- interfaceutil/src/main/java/com/daviscodesbugs/interfaceutil/InterfaceUtil.kt
+++ interfaceutil/src/main/java/com/daviscodesbugs/interfaceutil/InterfaceUtil.kt
@@ -4,8 +4,10 @@ package com.daviscodesbugs.interfaceutil
 
 import android.app.Activity
 import android.content.Context
+import android.content.DialogInterface
 import android.support.v4.app.Fragment
 import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 
 object InterfaceUtil {
 
@@ -17,6 +19,49 @@ object InterfaceUtil {
         val view = activity?.currentFocus
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
         imm?.hideSoftInputFromWindow(view?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+    }
+
+    fun confirmationDialog(
+        context: Context?,
+        title: String,
+        message: String,
+        confirm: String,
+        cancel: String,
+        handler: DialogInterface.OnClickListener
+    ): android.app.AlertDialog {
+        val builder = android.app.AlertDialog.Builder(context)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton(confirm, handler)
+        builder.setNegativeButton(cancel) { _, _ -> }
+        return builder.create()
+    }
+
+    fun confirmationDialog(
+        context: Context,
+        title: String,
+        message: String,
+        confirm: String,
+        cancel: String,
+        handler: DialogInterface.OnClickListener
+    ): android.support.v7.app.AlertDialog {
+        val builder = android.support.v7.app.AlertDialog.Builder(context)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton(confirm, handler)
+        builder.setNegativeButton(cancel) { _, _ -> }
+        return builder.create()
+    }
+
+    fun <T> makeDropDownAdapter(
+        context: Context?,
+        list: List<T>?
+    ): ArrayAdapter<T>? {
+        return context?.run {
+            val adapter = ArrayAdapter(this, R.layout.spinner_item, list ?: ArrayList())
+            adapter.setDropDownViewResource(R.layout.spinner_item)
+            adapter
+        }
     }
 
 }
